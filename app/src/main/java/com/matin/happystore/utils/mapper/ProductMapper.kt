4b656c319a9bf464869abd61c0c394f2859438b3
Yousef <matinfard.y@gmail.com>
2024-03-26 17:54:37 +0100

    Add product mapper

---
+++ app/src/main/java/com/matin/happystore/utils/mapper/ProductMapper.kt
@@ -0,0 +1,19 @@
+package com.matin.happystore.utils.mapper
+
+import com.matin.happystore.data.model.ProductEntity
+import com.matin.happystore.domain.model.Product
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class ProductMapper {
+    companion object {
+        fun toDomain(pn: ProductEntity): Product = Product(
+            pn.id,
+            pn.title.orEmpty(),
+            BigDecimal(pn.price ?: 0.0).setScale(2, RoundingMode.HALF_UP) ?: BigDecimal.ZERO,
+            pn.category.orEmpty(),
+            pn.description.orEmpty(),
+            pn.image.orEmpty()
+        )
+    }
+}
