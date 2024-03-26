0e570d680d37b2bf9dced31371b0c85756f3740c
Yousef <matinfard.y@gmail.com>
2024-03-26 17:55:56 +0100

    Add product item view

---
+++ app/src/main/java/com/matin/happystore/widgets/Widgets.kt
@@ -0,0 +1,101 @@
+package com.matin.happystore.widgets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.matin.happystore.R
+
+@Preview
+@Composable
+fun ProductItem() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(170.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(170.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.warrier),
+                    contentScale = ContentScale.FillBounds,
+                    contentDescription = null
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(6.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Image(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clip(
+                                CircleShape
+                            )
+                            .background(color = Color.LightGray)
+                            .padding(2.dp),
+                        colorFilter = ColorFilter.tint(Color.Red)
+                    )
+                }
+
+            }
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(modifier = Modifier.padding(top = 12.dp)) {
+                    Text(text = "Bold text first", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                    Text(text = "small text goes here")
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("$109.10", fontSize = 16.sp)
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(end = 8.dp)) {
+                        Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = null)
+                    }
+                }
+            }
+        }
+    }
+}
