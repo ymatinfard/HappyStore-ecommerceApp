8ac75662e89843d545168594f8196ecfe56a73b9
Yousef <matinfard.y@gmail.com>
2024-03-27 10:17:25 +0100

    Fix main progressBar size and position

--- app/src/main/java/com/matin/happystore/ui/MainActivity.kt
+++ app/src/main/java/com/matin/happystore/ui/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.matin.happystore.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.matin.happystore.ui.theme.HappyStoreTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,10 +26,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HappyStoreTheme {
-                // A surface container using the 'background' color from the theme
                 val viewModel = hiltViewModel<HappyStoreViewModel>()
                 val products = viewModel.products.collectAsState()
-                (products.value as? ProductUIState.Success)?.data?.get(0)?.let { Log.d("Yousef", it.image) }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     when (products.value) {
                         is ProductUIState.Success -> MainScreen((products.value as ProductUIState.Success).data)
                         is ProductUIState.Error -> Text(text = "Error" + (products.value as ProductUIState.Error).exception)
-                        is ProductUIState.Loading -> CircularProgressIndicator()
+                        is ProductUIState.Loading -> ProgressBar()
                     }
                 }
             }
@@ -42,18 +43,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HappyStoreTheme {
-        Greeting("Android")
+fun ProgressBar() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(modifier = Modifier.size(60.dp))
     }
 }
