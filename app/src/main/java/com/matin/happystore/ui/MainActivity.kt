d99b3fe6ce92fe4a02eae932aa132317d08b2b05
Yousef <matinfard.y@gmail.com>
2024-03-27 10:26:02 +0100

    Move MainProgressBar into Widgets class

--- app/src/main/java/com/matin/happystore/ui/MainActivity.kt
+++ app/src/main/java/com/matin/happystore/ui/MainActivity.kt
@@ -3,21 +3,15 @@ package com.matin.happystore.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.matin.happystore.ui.theme.HappyStoreTheme
+import com.matin.happystore.widgets.MainProgressBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,18 +29,10 @@ class MainActivity : ComponentActivity() {
                     when (products.value) {
                         is ProductUIState.Success -> MainScreen((products.value as ProductUIState.Success).data)
                         is ProductUIState.Error -> Text(text = "Error" + (products.value as ProductUIState.Error).exception)
-                        is ProductUIState.Loading -> ProgressBar()
+                        is ProductUIState.Loading -> MainProgressBar()
                     }
                 }
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun ProgressBar() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        CircularProgressIndicator(modifier = Modifier.size(60.dp))
     }
 }
--- app/src/main/java/com/matin/happystore/widgets/Widgets.kt
+++ app/src/main/java/com/matin/happystore/widgets/Widgets.kt
@@ -3,9 +3,11 @@ package com.matin.happystore.widgets
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -96,5 +100,13 @@ fun ProductItem(item: Product) {
                 }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun MainProgressBar() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(modifier = Modifier.size(60.dp))
     }
 }
