0f146128deb82f9905c89af4efc82aff61fa20a4
Yousef <matinfard.y@gmail.com>
2024-03-27 10:08:46 +0100

    Add show product items in a list

--- app/src/main/java/com/matin/happystore/ui/MainScreen.kt
+++ app/src/main/java/com/matin/happystore/ui/MainScreen.kt
@@ -1,8 +1,22 @@
 package com.matin.happystore.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.matin.happystore.domain.model.Product
+import com.matin.happystore.widgets.ProductItem
 
 @Composable
-fun MainScreen() {
+fun MainScreen(products: List<Product>) {
 
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(products) { product ->
+                ProductItem(product)
+            }
+        }
+    }
 }
