1eccd5144a07f27aa80f68473330c38e70fdf947
Yousef <matinfard.y@gmail.com>
2024-03-28 09:57:40 +0100

    Add product filter

--- app/src/main/java/com/matin/happystore/ui/MainScreen.kt
+++ app/src/main/java/com/matin/happystore/ui/MainScreen.kt
@@ -1,21 +1,32 @@
 package com.matin.happystore.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.matin.happystore.ui.model.UiProduct
+import com.matin.happystore.domain.model.Filter
+import com.matin.happystore.ui.model.UiProductState
+import com.matin.happystore.widgets.CategoryFilterChips
 import com.matin.happystore.widgets.ProductItem
 
 @Composable
-fun MainScreen(products: List<UiProduct>, onFavoriteClick: (Int) -> Unit, onProductClicked: (Int) -> Unit) {
+fun MainScreen(
+    uiProductState: UiProductState,
+    onFavoriteClick: (Int) -> Unit,
+    onProductClick: (Int) -> Unit,
+    onFilterClick: (Filter) -> Unit,
+) {
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            items(products) { product ->
-                ProductItem(product, onFavoriteClick, onProductClicked)
+        Column {
+            CategoryFilterChips(filters = uiProductState.filters, onFilterClick)
+            LazyColumn {
+                items(uiProductState.products) { product ->
+                    ProductItem(product, onFavoriteClick, onProductClick)
+                }
             }
         }
     }
