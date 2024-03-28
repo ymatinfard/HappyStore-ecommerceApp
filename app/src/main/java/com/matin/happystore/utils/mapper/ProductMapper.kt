9c848503301725b9a0cb6d554a6334a039c1b654
Yousef <matinfard.y@gmail.com>
2024-03-28 12:00:42 +0100

    Capitalise first character of category text

--- app/src/main/java/com/matin/happystore/utils/mapper/ProductMapper.kt
+++ app/src/main/java/com/matin/happystore/utils/mapper/ProductMapper.kt
@@ -11,7 +11,9 @@ class ProductMapper {
             pn.id,
             pn.title.orEmpty(),
             BigDecimal(pn.price ?: 0.0).setScale(2, RoundingMode.HALF_UP) ?: BigDecimal.ZERO,
-            pn.category.orEmpty(),
+            pn.category.orEmpty().replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase() else it.toString()
+            },
             pn.description.orEmpty(),
             pn.image.orEmpty()
         )
