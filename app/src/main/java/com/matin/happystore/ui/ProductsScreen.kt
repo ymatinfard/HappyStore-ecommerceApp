bff4ac7d9adfb5da0a2dc1c24a47ab8165b9cf0b
Yousef <matinfard.y@gmail.com>
2024-03-29 22:00:26 +0100

    Add ProductListReducerUseCase

--- app/src/main/java/com/matin/happystore/ui/ProductsScreen.kt
+++ app/src/main/java/com/matin/happystore/ui/ProductsScreen.kt
@@ -13,12 +13,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.matin.happystore.domain.model.Filter
-import com.matin.happystore.ui.model.ProductListScreenUiState
+import com.matin.happystore.ui.model.ProductsAndFilters
 import com.matin.happystore.ui.model.UiFilter
-import com.matin.happystore.ui.model.UiProduct
-import com.matin.happystore.ui.model.UiProductState
 import com.matin.happystore.widgets.CategoryFilterChips
 import com.matin.happystore.widgets.ProductItem
 import kotlinx.coroutines.flow.combine
@@ -26,40 +23,36 @@ import kotlinx.coroutines.flow.map
 
 
 @Composable
-fun ProductsScreen(viewModel: HappyStoreViewModel) {
-    val uiProductState = combine(
-        viewModel.store.state.map { it.products },
-        viewModel.store.state.map { it.favoriteProductId },
-        viewModel.store.state.map { it.expandedProductIds },
-        viewModel.store.state.map { it.productFilterInfo },
-        viewModel.store.state.map { it.inCartProductIds }) { products, favorites, expandeds, filterInfo, inCartProducts ->
+fun ProductsScreen(
+    viewModel: HappyStoreViewModel,
+) {
+    val productsScreenState =
+        combine(
+            viewModel.productListReducerUseCase.reduce(),
+            viewModel.store.state.map { it.productFilterInfo }) { products, filterInfo ->
 
-        if (products.isEmpty()) return@combine ProductListScreenUiState.Loading
+            if (products.isEmpty()) return@combine ProductsScreenUiState.Loading
 
-        val uiProducts =
-            products.filter { filterInfo.selectedFilter == null || it.category == filterInfo.selectedFilter.value }
-                .map {
-                    UiProduct(
-                        it,
-                        favorites.contains(it.id),
-                        expandeds.contains(it.id),
-                        inCartProducts.contains(it.id)
-                    )
+            val uiProducts =
+                products.filter {
+                    filterInfo.selectedFilter == null ||
+                            it.product.category == filterInfo.selectedFilter.value
                 }
-        val filters = filterInfo.filters.map { filter ->
-            UiFilter(filter, filter == filterInfo.selectedFilter)
-        }
 
-        ProductListScreenUiState.Success(
-            UiProductState(
+            val filters = filterInfo.filters.map { filter ->
+                UiFilter(filter, filter == filterInfo.selectedFilter)
+            }
+
+            val productsAndFilters = ProductsAndFilters(
                 uiProducts,
                 filters
             )
-        )
-    }.collectAsState(initial = ProductListScreenUiState.Loading)
+
+            ProductsScreenUiState.Success(productsAndFilters)
+        }.collectAsState(initial = ProductsScreenUiState.Loading)
 
     Surface {
-        ShowProductList(uiProductState = uiProductState.value,
+        HandleProductScreen(uiProductState = productsScreenState.value,
             onFavoriteClick = { productId ->
                 viewModel.updateFavoriteIds(productId)
             }, onProductClick = { productId ->
@@ -74,24 +67,24 @@ fun ProductsScreen(viewModel: HappyStoreViewModel) {
 }
 
 @Composable
-fun ShowProductList(
-    uiProductState: ProductListScreenUiState,
-    onFavoriteClick: (Int) -> Unit,
-    onProductClick: (Int) -> Unit,
-    onFilterClick: (Filter) -> Unit,
-    onAddToCartClick: (Int) -> Unit,
+fun HandleProductScreen(
+    uiProductState: ProductsScreenUiState,
+    onFavoriteClick: (Int) -> Unit = {},
+    onProductClick: (Int) -> Unit = {},
+    onFilterClick: (Filter) -> Unit = {},
+    onAddToCartClick: (Int) -> Unit = {},
 ) {
     when (uiProductState) {
-        is ProductListScreenUiState.Success -> ShowProductList(
-            uiProductState = uiProductState.data,
+        is ProductsScreenUiState.Success -> ShowProductList(
+            productsAndFilters = uiProductState.data,
             onFavoriteClick = onFavoriteClick,
             onProductClick = onProductClick,
             onFilterClick = onFilterClick,
             onAddToCartClick = onAddToCartClick
         )
-
-        is ProductListScreenUiState.Loading -> ShowLoading()
-        is ProductListScreenUiState.Error -> TODO()
+        is ProductsScreenUiState.Loading -> ShowLoading()
+        is ProductsScreenUiState.Error -> TODO()
+        else -> {}
     }
 }
 
@@ -104,7 +97,7 @@ fun ShowLoading() {
 
 @Composable
 fun ShowProductList(
-    uiProductState: UiProductState,
+    productsAndFilters: ProductsAndFilters,
     onFavoriteClick: (Int) -> Unit,
     onProductClick: (Int) -> Unit,
     onFilterClick: (Filter) -> Unit,
@@ -112,9 +105,9 @@ fun ShowProductList(
 ) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column {
-            CategoryFilterChips(filters = uiProductState.filters, onFilterClick)
+            CategoryFilterChips(filters = productsAndFilters.filters, onFilterClick)
             LazyColumn {
-                items(uiProductState.products) { product ->
+                items(productsAndFilters.products) { product ->
                     ProductItem(product, onFavoriteClick, onProductClick, onAddToCartClick)
                 }
             }
