795d17806227862f51c142418824c975972cb4e7
Yousef <matinfard.y@gmail.com>
2024-03-30 21:31:52 +0100

    Add product rating indicator

--- app/src/main/java/com/matin/happystore/utils/mapper/ProductMapper.kt
+++ app/src/main/java/com/matin/happystore/utils/mapper/ProductMapper.kt
@@ -2,20 +2,22 @@ package com.matin.happystore.utils.mapper
 
 import com.matin.happystore.data.model.ProductEntity
 import com.matin.happystore.domain.model.Product
+import com.matin.happystore.utils.orZero
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class ProductMapper {
     companion object {
-        fun toDomain(pn: ProductEntity): Product = Product(
-            pn.id,
-            pn.title.orEmpty(),
-            BigDecimal(pn.price ?: 0.0).setScale(2, RoundingMode.HALF_UP) ?: BigDecimal.ZERO,
-            pn.category.orEmpty().replaceFirstChar {
+        fun toDomain(pe: ProductEntity): Product = Product(
+            pe.id,
+            pe.title.orEmpty(),
+            BigDecimal(pe.price ?: 0.0).setScale(2, RoundingMode.HALF_UP) ?: BigDecimal.ZERO,
+            pe.category.orEmpty().replaceFirstChar {
                 if (it.isLowerCase()) it.titlecase() else it.toString()
             },
-            pn.description.orEmpty(),
-            pn.image.orEmpty()
+            pe.description.orEmpty(),
+            pe.image.orEmpty(),
+            Product.Rating(pe.rating?.rate.orZero(), pe.rating?.count.orZero())
         )
     }
 }
