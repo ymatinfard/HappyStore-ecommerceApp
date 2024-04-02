f4f85eee6a6e2b20ea0ac9385cc1bd1877563cdc
Yousef <matinfard.y@gmail.com>
2024-04-02 13:42:02 +0200

    Add test repository implementation

--- app/src/test/java/com/matin/happystore/TestHappyStoreRepository.kt
+++ app/src/test/java/com/matin/happystore/TestHappyStoreRepository.kt
@@ -8,7 +8,28 @@ import java.math.BigDecimal
 class TestHappyStoreRepository : HappyStoreRepository {
 
     override suspend fun getProducts(): Result<List<Product>> {
-        TODO()
+        val productsTest = listOf(
+            Product(
+                123,
+                "title1",
+                BigDecimal("23.3"),
+                "Jewerly",
+                "description1",
+                "http://example.png",
+                Product.Rating(3.4f, 1000)
+            ),
+            Product(
+                124,
+                "title2",
+                BigDecimal("24.4"),
+                "Jewerly",
+                "description2",
+                "http://example.png",
+                Product.Rating(4.4f, 2000)
+            )
+        )
+
+        return Result.Success(data = productsTest)
     }
 
     override suspend fun getSingleProduct(id: Int): Result<Product> {
