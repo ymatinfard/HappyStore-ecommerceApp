ced0202f3a5c24b2e5281d0695a20ee8a26e1cf2
Yousef <matinfard.y@gmail.com>
2024-04-02 13:49:31 +0200

    Rename some test case function name

--- app/src/test/java/com/matin/happystore/ui/stateupdater/ProductFavoriteUpdaterTest.kt
+++ app/src/test/java/com/matin/happystore/ui/stateupdater/ProductFavoriteUpdaterTest.kt
@@ -9,7 +9,7 @@ import java.math.BigDecimal
 class ProductFavoriteUpdaterTest {
 
     @Test
-    fun productIsFavorite() {
+    fun favoriteProductsShouldBeUpdatedAfterAddingNewFavorite() {
         val productFavoriteUpdater = ProductFavoriteUpdater()
         val productsTest = listOf(
             Product(
