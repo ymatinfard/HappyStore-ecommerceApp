ce223b7d84ffe48d6d97c05130761a09e3628480
Yousef <matinfard.y@gmail.com>
2024-04-04 19:08:23 +0200

    Reset product quantity when removed from shopping cart

--- app/src/main/java/com/matin/happystore/ui/stateupdater/ProductInCartItemUpdater.kt
+++ app/src/main/java/com/matin/happystore/ui/stateupdater/ProductInCartItemUpdater.kt
@@ -6,13 +6,23 @@ import javax.inject.Inject
 class ProductInCartItemUpdater @Inject constructor() {
 
     operator fun invoke(id: Int, appState: ApplicationState): ApplicationState {
-        val currentInCartProducts = appState.inCartProductIds
+        val currentInCartProductIds = appState.inCartProductIds
+        val currentInCartProductQuantity = appState.inCartProductQuantity
+        val newInCartProducts: Set<Int>
+        val updatedInCartProductQuantity: Map<Int, Int>
+
+        if (currentInCartProductIds.contains(id)) {
+            newInCartProducts = currentInCartProductIds - id
+            // Reset quantity value if deleted from cart
+            updatedInCartProductQuantity = currentInCartProductQuantity + (id to 1)
+        } else {
+            newInCartProducts = currentInCartProductIds + id
+            updatedInCartProductQuantity = currentInCartProductQuantity
+        }
+
         return appState.copy(
-            inCartProductIds = if (currentInCartProducts.contains(id)) {
-                currentInCartProducts - id
-            } else {
-                currentInCartProducts + id
-            }
+            inCartProductIds = newInCartProducts,
+            inCartProductQuantity = updatedInCartProductQuantity
         )
     }
 }
