ac4d7e7310bfc7caa38b437a86554007e36e17af
Yousef <matinfard.y@gmail.com>
2024-04-04 19:07:44 +0200

    Align TotalCartItemsPrice to the bottom of CartScreen

--- app/src/main/java/com/matin/happystore/ui/screen/CartScreen.kt
+++ app/src/main/java/com/matin/happystore/ui/screen/CartScreen.kt
@@ -1,6 +1,9 @@
 package com.matin.happystore.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -9,10 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.matin.happystore.ui.CartScreenUiState
 import com.matin.happystore.ui.viewmodel.HappyStoreViewModel
 import com.matin.happystore.ui.model.UiProduct
 import com.matin.happystore.ui.widgets.CartItem
+import com.matin.happystore.ui.widgets.TotalCartItemsPrice
 
 @Composable
 fun CartScreen(viewModel: HappyStoreViewModel) {
@@ -24,7 +29,7 @@ fun CartScreen(viewModel: HappyStoreViewModel) {
         HandleCartScreen(inCartProductsState.value, onFavoriteClick = { favoriteItemId ->
             viewModel.updateFavoriteIds(favoriteItemId)
         }, onDeleteClick = { deletedItemId ->
-            viewModel.updateInCartIds(deletedItemId)
+            viewModel.updateInCartItemIds(deletedItemId)
         }, onQuantityChange = { productId, newQuantity ->
             viewModel.updateProductInCartQuantity(productId, newQuantity)
         })
@@ -57,14 +62,23 @@ fun ShowCartItems(
     onDeleteClick: (Int) -> Unit,
     onQuantityChange: (Int, Int) -> Unit,
 ) {
-    LazyColumn {
-        items(cartItems) { item ->
-            CartItem(
-                item = item,
-                onFavoriteClick = onFavoriteClick,
-                onDeleteClick = onDeleteClick,
-                onQuantityChange = onQuantityChange
-            )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 50.dp)
+            .background(color = MaterialTheme.colorScheme.background)
+    ) {
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            items(cartItems) { item ->
+                CartItem(
+                    item = item,
+                    onFavoriteClick = onFavoriteClick,
+                    onDeleteClick = onDeleteClick,
+                    onQuantityChange = onQuantityChange
+                )
+            }
         }
+
+        TotalCartItemsPrice(cartItems)
     }
 }
