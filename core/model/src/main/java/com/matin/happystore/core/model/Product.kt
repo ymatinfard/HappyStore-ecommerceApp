d6806195d808ae7ed4b165e74d9ccbffd2bffc13
Yousef <matinfard.y@gmail.com>
2024-04-07 21:47:46 +0200

    Add model module

---
+++ core/model/src/main/java/com/matin/happystore/core/model/Product.kt
@@ -0,0 +1,18 @@
+package com.matin.happystore.core.model
+
+import java.math.BigDecimal
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: BigDecimal,
+    val category: String,
+    val description: String,
+    val image: String,
+    val rating: Rating,
+) {
+    data class Rating (
+        val rate: Float,
+        val count: Int
+    )
+}
