0611a08fc981c91413b1b34500da36d81ee47ecc
Yousef <matinfard.y@gmail.com>
2024-04-08 18:45:40 +0200

    Add design system module packages

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.happystore.android.library)
+    alias(libs.plugins.happystore.android.library.compose)
+}
+
+android {
+    namespace = "com.matin.happystore.core.designsystem"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material3)
+    implementation(libs.coil.kt.compose)
+}
