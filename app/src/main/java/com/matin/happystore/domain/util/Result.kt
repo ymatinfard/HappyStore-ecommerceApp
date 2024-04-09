c244a7d00aa2aa4702695ebd115d8d261cfa7eb0
Yousef <matinfard.y@gmail.com>
2024-04-09 15:37:47 +0200

    Remove files from app folder after moving them into related modules

--- app/src/main/java/com/matin/happystore/domain/util/Result.kt
+++
@@ -1,6 +0,0 @@
-package com.matin.happystore.domain.util
-
-sealed interface Result<T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error<Nothing>(val exception: Throwable) : Result<Nothing>
-}
