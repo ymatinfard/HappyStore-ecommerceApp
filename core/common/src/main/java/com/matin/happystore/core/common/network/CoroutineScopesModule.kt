5e1798ca19bf0b0e752cafd7b5f8d5bb4ef2c635
Yousef <matinfard.y@gmail.com>
2024-04-16 14:14:53 +0200

    Add CoroutineScope dependency module

---
+++ core/common/src/main/java/com/matin/happystore/core/common/network/CoroutineScopesModule.kt
@@ -0,0 +1,26 @@
+package com.matin.happystore.core.common.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object CoroutineScopesModule {
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun providesCoroutineScope(
+        @Dispatcher(HappyStoreDispatchers.Default) dispatcher: CoroutineDispatcher,
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
+}
