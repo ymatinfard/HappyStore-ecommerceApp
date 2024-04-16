ab71ed6c6467c101be60a55aa52cf8139bf3e0bf
Yousef <matinfard.y@gmail.com>
2024-04-16 14:13:35 +0200

    Add asResource extension to map result to Result class

--- core/common/src/main/java/com/matin/happystore/core/common/Result.kt
+++ core/common/src/main/java/com/matin/happystore/core/common/Result.kt
@@ -1,6 +1,17 @@
 package com.matin.happystore.core.common
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
+
 sealed interface Result<T> {
     data class Success<T>(val data: T) : Result<T>
     data class Error<Nothing>(val exception: Throwable) : Result<Nothing>
+}
+
+fun <T> Flow<T>.asResource(): Flow<Result<T>> = map<T, Result<T>> {
+    Result.Success(it)
+}.catch {
+    emit(Result.Error(it))
 }
