b4280cd4d8f5a39a01eb0b0ad61746dd42669922
Yousef <matinfard.y@gmail.com>
2024-04-16 09:55:09 +0200

    Add room library

--- core/database/src/main/java/com/matin/happystore/core/database/model/HappyStoreDatabase.kt
+++ core/database/src/main/java/com/matin/happystore/core/database/model/HappyStoreDatabase.kt
@@ -2,9 +2,11 @@ package com.matin.happystore.core.database.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.matin.happystore.core.database.model.di.ProductsDao
 
 @Database(entities = [ProductEntity::class], version = 1)
-abstract class HappyStoreDatabase: RoomDatabase() {
-    abstract fun productsDao() : ProductsDao
+@TypeConverters(BigDecimalTypeConvertor::class)
+abstract class HappyStoreDatabase : RoomDatabase() {
+    abstract fun productsDao(): ProductsDao
 }
