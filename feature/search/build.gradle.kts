65242e66df3914311d94310ec20e0d97deb45337
Yousef <matinfard.y@gmail.com>
2024-06-24 15:53:47 +0200

    Add search module
    - Added only the module packages

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.happystore.android.feature)
+    alias(libs.plugins.happystore.android.library.compose)
+}
+
+android {
+    namespace = "com.matin.happystore.feature.search"
+}
+
+dependencies {
+
+    implementation(projects.core.ui)
+    implementation(projects.core.domain)
+    implementation(projects.core.model)
+    implementation(projects.core.common)
+    implementation(projects.core.testing)
+}
---
+++ feature/search/src/main/java/com/matin/happystore/feature/search/Search.kt
@@ -0,0 +1,9 @@
+package com.matin.happystore.feature.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Search() {
+    Text("Search screen")
+}
