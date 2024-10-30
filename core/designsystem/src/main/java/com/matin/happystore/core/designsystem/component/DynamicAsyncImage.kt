41c705542cf368786e721f11e2f007067434b89e
Yousef <matinfard.y@gmail.com>
2024-10-30 16:26:26 +0100

    Refactor AsyncImage to a custom image loading class

---
+++ core/designsystem/src/main/java/com/matin/happystore/core/designsystem/component/DynamicAsyncImage.kt
@@ -0,0 +1,60 @@
+package com.matin.happystore.core.designsystem.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter.State.Error
+import coil.compose.AsyncImagePainter.State.Loading
+import coil.compose.rememberAsyncImagePainter
+import com.matin.happystore.core.designsystem.R
+
+@Composable
+fun DynamicAsyncImage(
+    imageUrl: String,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    placeholder: Painter = painterResource(R.drawable.place_holder),
+) {
+    var isLoading by remember { mutableStateOf(true) }
+    var isError by remember { mutableStateOf(false) }
+    val imageLoader = rememberAsyncImagePainter(
+        model = imageUrl,
+        onState = { state ->
+            isLoading = state is Loading
+            isError = state is Error
+        },
+    )
+    val isLocalInspection = LocalInspectionMode.current
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        if (isLoading && !isLocalInspection) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(80.dp),
+                color = MaterialTheme.colorScheme.tertiary,
+            )
+        }
+        Image(
+            contentScale = ContentScale.Crop,
+            painter = if (isError.not() && !isLocalInspection) imageLoader else placeholder,
+            contentDescription = contentDescription,
+        )
+    }
+}
--- core/ui/src/main/java/com/matin/happystore/core/ui/components/ProductItem.kt
+++ core/ui/src/main/java/com/matin/happystore/core/ui/components/ProductItem.kt
@@ -23,13 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
+import com.matin.happystore.core.designsystem.component.DynamicAsyncImage
 import com.matin.happystore.core.designsystem.component.LoadingWheel
 import com.matin.happystore.core.designsystem.theme.AppTypography
 import com.matin.happystore.core.model.Product
@@ -137,14 +136,10 @@ fun ProductCard(item: UiProduct, onFavoriteClick: (Int) -> Unit) {
         elevation = CardDefaults.cardElevation(6.dp)
     ) {
         Box {
-            AsyncImage(
-                model = item.product.image,
-                contentDescription = null,
+            DynamicAsyncImage(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(padding),
-                contentScale = ContentScale.FillBounds,
-                onState = { state -> showLoading = state is AsyncImagePainter.State.Loading }
+                    .padding(padding), imageUrl = item.product.image, contentDescription = "image"
             )
 
             if (showLoading) {
