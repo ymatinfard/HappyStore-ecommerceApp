214027c77b61e206acb4f65a039df5faa0cda59b
Yousef <matinfard.y@gmail.com>
2024-10-30 14:15:58 +0100

    Remove default statusbar color

--- core/designsystem/src/main/java/com/matin/happystore/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/matin/happystore/core/designsystem/theme/Theme.kt
@@ -273,15 +273,6 @@ fun HappyStoreTheme(
       darkTheme -> darkScheme
       else -> lightScheme
   }
-  val view = LocalView.current
-  if (!view.isInEditMode) {
-    SideEffect {
-      val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-    }
-  }
-
   MaterialTheme(
     colorScheme = colorScheme,
     typography = AppTypography,
