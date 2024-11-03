dc86c6cf601796088c72c239cb795d849ed62e09
Yousef <matinfard.y@gmail.com>
2024-11-03 17:45:01 +0100

    Get single product form DB along with its cart status

--- core/domain/src/main/java/com/matin/happystore/core/domain/GetSingleProductUseCase.kt
+++ core/domain/src/main/java/com/matin/happystore/core/domain/GetSingleProductUseCase.kt
@@ -1,13 +1,25 @@
 package com.matin.happystore.core.domain
 
 import com.matin.data.HappyStoreRepository
-import com.matin.happystore.core.model.Product
+import com.matin.happystore.core.model.ui.UiProduct
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetSingleProductUseCase
 @Inject
 constructor(private val repository: HappyStoreRepository) {
-    operator fun invoke(id: Int): Flow<Product> = repository.getSingleProduct(id)
-
+    operator fun invoke(id: Int): Flow<UiProduct> {
+        return combine(
+            repository.getSingleProduct(id),
+            repository.getInCartProductIds(),
+            ::Pair
+        ).map { productToCart ->
+            UiProduct(
+                product = productToCart.first, isInCart = productToCart
+                    .second.contains(productToCart.first.id), isExpended = false, isFavorite = false
+            )
+        }
+    }
 }
