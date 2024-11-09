6661f7c3b75dda432f3f66b162cb7364e8a8cd79
Yousef <matinfard.y@gmail.com>
2024-11-09 15:46:14 +0100

    Add gradient modifier extension

---
+++ core/designsystem/src/main/java/com/matin/happystore/core/designsystem/component/Gradient.kt
@@ -0,0 +1,87 @@
+package com.matin.happystore.core.designsystem.component
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun Modifier.diagonalGradientTint(
+    colors: List<Color>,
+    blendMode: BlendMode
+) = drawWithContent {
+    drawContent()
+    drawRect(
+        brush = Brush.linearGradient(colors),
+        blendMode = blendMode
+    )
+}
+
+fun Modifier.offsetGradientBackground(
+    colors: List<Color>,
+    width: Float,
+    offset: Float = 0f
+) = background(
+    Brush.horizontalGradient(
+        colors = colors,
+        startX = -offset,
+        endX = width - offset,
+        tileMode = TileMode.Mirror
+    )
+)
+
+fun Modifier.offsetGradientBackground(
+    colors: List<Color>,
+    width: Density.() -> Float,
+    offset: Density.() -> Float = { 0f }
+) = drawBehind {
+    val actualOffset = offset()
+
+    drawRect(
+        Brush.horizontalGradient(
+            colors = colors,
+            startX = -actualOffset,
+            endX = width() - actualOffset,
+            tileMode = TileMode.Mirror
+        )
+    )
+}
+
+fun Modifier.diagonalGradientBorder(
+    colors: List<Color>,
+    borderSize: Dp = 2.dp,
+    shape: Shape
+) = border(
+    width = borderSize,
+    brush = Brush.linearGradient(colors),
+    shape = shape
+)
+
+fun Modifier.fadeInDiagonalGradientBorder(
+    showBorder: Boolean,
+    colors: List<Color>,
+    borderSize: Dp = 2.dp,
+    shape: Shape
+) = composed {
+    val animatedColors = List(colors.size) { i ->
+        animateColorAsState(
+            if (showBorder) colors[i] else colors[i].copy(alpha = 0f),
+            label = "animated color"
+        ).value
+    }
+    diagonalGradientBorder(
+        colors = animatedColors,
+        borderSize = borderSize,
+        shape = shape
+    )
+}
